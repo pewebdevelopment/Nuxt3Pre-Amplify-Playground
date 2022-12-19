fe8f8e4d8f9bf2aa759e2f5cef3e89600ddeefc7
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-12-19 21:54:22 +0530

    Paper Working Preliminarily

--- components/Whiteboard/WhiteBoard.vue
+++ components/Whiteboard/WhiteBoard.vue
@@ -22,9 +22,29 @@ export default {
 
   mounted() {
     if (process.browser) {
-      // console.log("The window object:", window);
-      paper.install(window);
+      console.log("Process - Browser");
+    }
+    if (process.client) {
+      console.log("Process - Client");
+      // paper.install(window);
+      // paper.setup(document.getElementById("canvas-id"));
+    }
+
+    if (document.getElementById("canvas-id")) {
+      // paper.install(window);
       paper.setup(document.getElementById("canvas-id"));
+
+      // Create a Paper.js Path to draw a line into it:
+      console.log("Mounted Whiteboard");
+      var path = new paper.Path();
+      // Give the stroke a color
+      path.strokeColor = "black";
+      var start = new paper.Point(100, 100);
+      // Move to start and draw a line from there
+      path.moveTo(start);
+      // Note the plus operator on Point objects.
+      // PaperScript does that for us, and much more!
+      path.lineTo(start + [100, -50]);
     }
   },
 };
