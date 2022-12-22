bf6de775ed87ae65d0ae21666a43eafe2f4f5766
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-12-22 17:55:07 +0530

    getting tasks from the API

---
+++ pages/PE/tasks.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>{{ studentTasks }}</div>
+</template>
+
+<script setup>
+import { reactive, toRefs } from "vue";
+import { useTasksAPIStore } from "~~/stores/tasksAPI";
+
+// const state = reactive({
+//   count: 17,
+// });
+
+const TasksList = useTasksAPIStore();
+console.log("TasksList:", TasksList);
+
+const getStudentLists = async () => {
+  const response = await TasksList.listStudentTasks();
+  console.log("Response:", response);
+  return response;
+};
+
+onMounted(() => {
+  getStudentLists();
+});
+
+const studentTasks = reactive(TasksList);
+</script>
+
+<style scoped></style>
