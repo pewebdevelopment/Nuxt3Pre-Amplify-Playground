9122068f554bbdf1347b4cbdf74874e6e5698ee9
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-12-22 18:41:29 +0530

    tasks

--- pages/PE/tasks.vue
+++ pages/PE/tasks.vue
@@ -1,10 +1,31 @@
 <template>
-  <div>{{ studentTasks }}</div>
+  <div>
+    <NuxtLayout name="zen">
+      <SectionMain>
+        <CardBox has-table>
+          <TableTasks :tasks="studentTasks.tasksList" />
+          <!-- {{ studentTasks.tasksList }} -->
+        </CardBox>
+      </SectionMain>
+    </NuxtLayout>
+  </div>
 </template>
 
 <script setup>
 import { reactive, toRefs } from "vue";
 import { useTasksAPIStore } from "~~/stores/tasksAPI";
+import { computed, ref, onMounted } from "vue";
+import TableTasks from "@/components/TableTasks.vue";
+
+import {
+  mdiAccountMultiple,
+  mdiCartOutline,
+  mdiChartTimelineVariant,
+  mdiMonitorCellphone,
+  mdiReload,
+  mdiGithub,
+  mdiChartPie,
+} from "@mdi/js";
 
 // const state = reactive({
 //   count: 17,
@@ -15,15 +36,23 @@ console.log("TasksList:", TasksList);
 
 const getStudentLists = async () => {
   const response = await TasksList.listStudentTasks();
-  console.log("Response:", response);
+  console.log("Response:", response.data.listStudentTasks.items);
+  studentTasks.tasksList = response.data.listStudentTasks.items;
+  console.log(" studentTasks.tasksList:", studentTasks.tasksList[0]);
   return response;
 };
 
 onMounted(() => {
+  //   console.log(
+  //     "Response within onMounted:",
+  //     studentTasks.data.listStudentTasks.items
+  //   );
   getStudentLists();
 });
 
-const studentTasks = reactive(TasksList);
+const studentTasks = reactive({
+  tasksList: [{ title: "abc" }],
+});
 </script>
 
 <style scoped></style>
