70d761cbfdda75fa7a2c11b398ad319401e6107a
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-12-26 23:24:42 +0530

    Pages structure changed

--- pages/index.vue
+++ pages/index.vue
@@ -4,25 +4,22 @@ import { mdiAccount, mdiAsterisk } from "@mdi/js";
 import { useRouter } from "vue-router";
 
 const form = reactive({
-  login: "john.doe",
+  login: "Zenith.Athang",
   pass: "highly-secure-password-fYjUw-",
   remember: true,
 });
 const router = useRouter();
 const submit = () => {
-router.push("/dashboard");
+  router.push("/dashboard");
 };
 </script>
 
 <template>
   <div>
-  <NuxtLayout>
-
-    <SectionFullScreen v-slot="{ cardClass }" bg="purplePink">
-      <CardBox :class="cardClass" is-form @submit.prevent="submit">
-
-
-        <FormField label="Login" help="Please enter your login">
+    <NuxtLayout>
+      <SectionFullScreen v-slot="{ cardClass }" bg="purplePink">
+        <CardBox :class="cardClass" is-form @submit.prevent="submit">
+          <FormField label="Login" help="Please enter your login">
             <FormControl
               v-model="form.login"
               :icon="mdiAccount"
@@ -31,7 +28,7 @@ router.push("/dashboard");
             />
           </FormField>
 
-        <FormField label="Password" help="Please enter your password">
+          <FormField label="Password" help="Please enter your password">
             <FormControl
               v-model="form.pass"
               :icon="mdiAsterisk"
@@ -41,27 +38,21 @@ router.push("/dashboard");
             />
           </FormField>
 
-        <FormCheckRadio
+          <FormCheckRadio
             v-model="form.remember"
             name="remember"
             label="Remember"
             :input-value="true"
           />
 
-
-        <template #footer>
+          <template #footer>
             <BaseButtons>
               <BaseButton type="submit" color="info" label="Login" />
               <BaseButton to="/dashboard" color="info" outline label="Back" />
             </BaseButtons>
           </template>
-      </CardBox>
-
-    </SectionFullScreen>
-
-
-
-  </NuxtLayout>
-
-</div>
+        </CardBox>
+      </SectionFullScreen>
+    </NuxtLayout>
+  </div>
 </template>
