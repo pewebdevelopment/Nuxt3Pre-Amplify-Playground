5cda324e453762e427de0dffaf4d7182ea5145bb
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-12-27 00:26:20 +0530

    import statements added in the index page

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,14 @@ import { reactive } from "vue";
 import { mdiAccount, mdiAsterisk } from "@mdi/js";
 import { useRouter } from "vue-router";
 
+import SectionFullScreen from "@/components/Sections/SectionFullScreen.vue";
+import CardBox from "@/components/Cards/CardBox.vue";
+import FormField from "@/components/Forms/FormField.vue";
+import FormControl from "@/components/Forms/FormControl.vue";
+import FormCheckRadio from "@/components/Forms/FormCheckRadio.vue";
+import BaseButtons from "@/components/Buttons/BaseButtons.vue";
+import BaseButton from "@/components/Buttons/BaseButton.vue";
+
 const form = reactive({
   login: "Zenith.Athang",
   pass: "highly-secure-password-fYjUw-",
