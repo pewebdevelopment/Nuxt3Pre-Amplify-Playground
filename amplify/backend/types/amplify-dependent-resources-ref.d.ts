e0cddfeb4ed3e3f5cbbbecaccc681579d76a8e66
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2023-02-04 22:04:44 +0530

    @index removed from the files

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,25 +1,25 @@
 export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "nuxt3ampplayground": {
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    },
-    "auth": {
-        "Nuxt3AmpPlayGround": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string"
-        }
-    },
-    "storage": {
-        "s3nuxt3ampplaygroundstorage80ebce9c": {
-            "BucketName": "string",
-            "Region": "string"
-        }
+  "api": {
+    "nuxt3ampplayground": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
     }
+  },
+  "auth": {
+    "Nuxt3AmpPlayGround": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3nuxt3ampplaygroundstorage80ebce9c": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
 }
